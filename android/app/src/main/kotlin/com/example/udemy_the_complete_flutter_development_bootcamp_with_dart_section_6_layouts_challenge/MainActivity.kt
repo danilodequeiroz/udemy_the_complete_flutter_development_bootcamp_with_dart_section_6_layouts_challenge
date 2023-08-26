98b3921a35a926fd642c1cdaee29a794b0986c1b
danilodequeiroz <danilo.aq@gmail.com>
2023-08-25 23:28:36 -0300

    creates new blank project;

---
+++ android/app/src/main/kotlin/com/example/udemy_the_complete_flutter_development_bootcamp_with_dart_section_6_layouts_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy_the_complete_flutter_development_bootcamp_with_dart_section_6_layouts_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
